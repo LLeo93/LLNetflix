e961c2e9047e8525479e35cc99d19254eedf4912
LLeo93 <liba.leoncini@gmail.com>
2025-04-04 16:36:42 +0200

    messaggio alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import CustomNavbar from './components/CustomNavbar';
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 import FilmGallery from './components/Galleria';
 import Settings from './components/Settings';
 import UserProfile from './components/Profilo';
+import HomeAlert from './components/HomeAlert';
 
 function App() {
   return (
@@ -20,6 +21,14 @@ function App() {
               path="/"
               element={
                 <>
+                  <HomeAlert
+                    message="Ciao Stefano! questo alert sparirà tra 20 secondi o al tuo click della x. 
+                  la pagina è responsive e ci ho collegato profile page e setting page,
+                   i pulsanti dinamici sono home profilo settings le foto e il carosello,
+                    in mobile pure i dropdown e il pulsante osceno col bg grigio bruttissimo
+                    le opzioni delle altre pagine sono dinamiche e il carosello mostra meno slide 
+                    in base ai breakpoints"
+                  />
                   <h1 className="text-white my-4">Film In Evidenza</h1>
                   <div className="mb-md-3 mb-lg-5 mb-sm-2">
                     <FilmGallery
@@ -39,10 +48,12 @@ function App() {
                 </>
               }
             />
+
             <Route path="/settings" element={<Settings />} />
             <Route path="/user-profile" element={<UserProfile />} />
           </Routes>
         </div>
+
         <Footer />
       </main>
     </Router>
